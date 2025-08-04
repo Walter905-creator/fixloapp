64c70b895b44d50926f14bd7a899480f5425d7b0
Walter905-creator <pro4u.improvements@gmail.com>
2025-08-04 19:27:34 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",     // ✅ Your main React components
-    "./public/index.html"             // ✅ Your main HTML file
+    "./src/**/*.{js,jsx,ts,tsx}",     
+    "./public/index.html"             
   ],
   theme: {
     extend: {},
