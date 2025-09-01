ce3945ebc3d0b873ac49347e05047ed72b0a7d69
Walter905-creator <pro4u.improvements@gmail.com>
2025-09-01 16:33:04 -0400

    Add comment to Analytics component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import App from './App';
 import { Analytics } from '@vercel/analytics/react';
+import App from './App';
 import './index.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
         <App />
-        <Analytics />     
+        <Analytics /> {/* Vercel Web Analytics */}
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
